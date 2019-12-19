9ebb326949e816afe3fdf2166d17a1e3c48339e1
Stephan <stephan.pirner93@gmail.com>
2019-12-19 16:47:32 +0100

    fix database column error

--- heating_monitor/temp_monitor/utils.py
+++ heating_monitor/temp_monitor/utils.py
@@ -18,26 +18,29 @@ def check_log_directory():
 
 def write_temps_to_logs():
     check_log_directory()
-    sensors = models.TempSensor.objects.all()
-    for sensor in sensors:
-        try:
-            sensor_file = os.path.join('/','sys', 'bus', 'w1', 'devices', sensor.hw_id, 'w1_slave')
-            f = open(sensor_file, 'r')
-            lines = f.readlines()
-            f.close()
-            temp_str = lines[1].find('t=')
-            temp_cels = 0
-            # check if temperature was written
-            if temp_str != -1 :
-                temp_data = lines[1][temp_str+2:]
-                temp_cels = float(temp_data) / 1000.0
-                sensor.last_temperature = temp_cels
-                sensor.save()
-            else:
+    try:
+        sensors = models.TempSensor.objects.all()
+        for sensor in sensors:
+            try:
+                sensor_file = os.path.join('/','sys', 'bus', 'w1', 'devices', sensor.hw_id, 'w1_slave')
+                f = open(sensor_file, 'r')
+                lines = f.readlines()
+                f.close()
+                temp_str = lines[1].find('t=')
+                temp_cels = 0
+                # check if temperature was written
+                if temp_str != -1 :
+                    temp_data = lines[1][temp_str+2:]
+                    temp_cels = float(temp_data) / 1000.0
+                    sensor.last_temperature = temp_cels
+                    sensor.save()
+                else:
+                    sensor.last_temperature = -1.0
+                    sensor.save()
+                print('temperature is: ', temp_cels)
+            except Exception as sub_e:
                 sensor.last_temperature = -1.0
                 sensor.save()
-            print('temperature is: ', temp_cels)
-        except Exception as e:
-            sensor.last_temperature = -1.0
-            sensor.save()
-            print(e)
+                print(sub_e)
+    except Exception as e:
+        print(e)
