0cd09625a259b35045ffe3747758117232445e71
Stephan <stephan.pirner93@gmail.com>
2019-12-20 21:33:57 +0100

    view test

--- heating_monitor/temp_monitor/utils.py
+++ heating_monitor/temp_monitor/utils.py
@@ -44,3 +44,44 @@ def write_temps_to_logs():
                 print(sub_e)
     except Exception as e:
         print(e)
+
+def check_all_w1_devices():
+    check_log_directory()
+    try:
+        # sensors = models.TempSensor.objects.all()
+        w1_dir = os.path.join('/', 'sys', 'bus', 'w1', 'devices')
+        entries = os.listdir(w1_dir)
+        # filter entries for useless
+        filtered_entries = []
+        for entry in entries:
+            if not ('w1_bus' in entry):
+                filtered_entries.append(entry)
+
+        print('\n\nfiltered entries')
+        print(filtered_entries)
+        print('\n\n')
+
+        for entry in entries:
+            try:
+                sensor_file = os.path.join('/','sys', 'bus', 'w1', 'devices', entry, 'w1_slave')
+                f = open(sensor_file, 'r')
+                lines = f.readlines()
+                f.close()
+                temp_str = lines[1].find('t=')
+                temp_cels = 0
+                # check if temperature was written
+                if temp_str != -1 :
+                    temp_data = lines[1][temp_str+2:]
+                    temp_cels = float(temp_data) / 1000.0
+                    sensor.last_temperature = temp_cels
+                    sensor.save()
+                else:
+                    sensor.last_temperature = -1.0
+                    sensor.save()
+                print('temperature is: ', temp_cels)
+            except Exception as sub_e:
+                sensor.last_temperature = -1.0
+                sensor.save()
+                print(sub_e)
+    except Exception as e:
+        print(e)
