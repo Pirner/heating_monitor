58455013458fc17200bd1a76cbccf2edcea311e3
Stephan <stephan.pirner93@gmail.com>
2019-12-27 16:48:10 +0100

    broken matplotlib

--- heating_monitor/temp_monitor/views.py
+++ heating_monitor/temp_monitor/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from matplotlib import pylab
-from pylab import *
+# from matplotlib import pylab
+# from pylab import *
 import PIL, PIL.Image
 from django.views.generic.list import ListView
 from io import StringIO, BytesIO
@@ -51,25 +51,34 @@ def view_sensors(request):
 def temp_plot(request):
     # Construct the graph
     utils.write_temps_to_logs()
-    x = arange(0, 2*pi, 0.01)
-    s = cos(x)**2
-    plot(x, s)
+    # x = arange(0, 2*pi, 0.01)
+    # s = cos(x)**2
+    # plot(x, s)
 
-    xlabel('xlabel(X)')
-    ylabel('ylabel(Y)')
-    title('Simple Graph!')
-    grid(True)
+    #xlabel('xlabel(X)')
+    #ylabel('ylabel(Y)')
+    #title('Simple Graph!')
+    #grid(True)
     # Store image in a string buffer
     # buffer = StringIO()
     # from pudb import set_trace
     # set_trace()
-    buffer = BytesIO()
-    canvas = pylab.get_current_fig_manager().canvas
-    canvas.draw()
+    #buffer = BytesIO()
+    #canvas = pylab.get_current_fig_manager().canvas
+    #canvas.draw()
     # pilImage = PIL.Image.fromstring("RGB", canvas.get_width_height(), canvas.tostring_rgb())
-    pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
-    pilImage.save(buffer, "PNG")
-    pylab.close()
+    #pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
+    # pilImage.save(buffer, "PNG")
+    # pylab.close()
 
     # Send buffer in a http response the the browser with the mime type image/png set
-    return HttpResponse(buffer.getvalue(), content_type="image/png")
+    # return HttpResponse(buffer.getvalue(), content_type="image/png")
+    sensor_vals = utils.check_all_w1_devices()
+    x = 5
+    template = loader.get_template('view_sensors.html')
+    context = {
+        'x': x,
+        'sensors': sensor_vals,
+    }
+    return HttpResponse(template.render(context, request))
+
